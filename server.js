ecfe2229bd73ce1fd29c4c068d7a96347b9aacf1
Your Name <you@example.com>
2025-05-20 02:42:20 +0200

    commit

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const path = require("path");
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 /* ***********************
  * Create Express App
@@ -35,15 +38,58 @@ app.set("views", path.join(__dirname, "views")); // Path to views
 /* ***********************
  * Routes
  *************************/
-// Root route - render views/index.ejs
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
-});
+// Index route
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  console.dir(err);
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Start Server
  *************************/
-const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+
