3aaba2a11dfdb82ce9f87acaad18e18086badb10
Your Name <you@example.com>
2025-06-06 10:11:17 +0200

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,16 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const navLinks = document.querySelector('nav ul');
 
-hamburgerIcon.addEventListener('click', () => {
-  console.log('Hamburger clicked!'); // Add this line
-  navLinks.classList.toggle('open');
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("menu-toggle");
+    const nav = document.getElementById("primary-nav");
+
+    toggleBtn.addEventListener("click", () => {
+      nav.classList.toggle("open");
+      nav.classList.toggle("closed");
+
+      const expanded = toggleBtn.getAttribute("aria-expanded") === "true";
+      toggleBtn.setAttribute("aria-expanded", !expanded);
+    });
+  });
+
+
 
